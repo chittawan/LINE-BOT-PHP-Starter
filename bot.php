7297e8ccdb38aae8e8277d3d09b9188ce44ac5e8
chittawan <Associa_@hotmail.com>
2017-02-24 16:38:45 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -11,37 +11,69 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get User 
+			if ($event['source']['type'] == 'user') {
+				$userId = $event['source']['userId'];			
+			}
+			elseif ($event['source']['type'] == 'group') {
+				$userId = $event['source']['groupId'];			
+			}
+
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			if (stripos($text, "©та") !== false) {
+	
+				if ($userId == $SSCGroupId || $userId == $TestGroupId || $userId == $PaeUserId) {
+					$messages = GetReplyMessage($text,$userId);
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				} 
+				elseif (stripos($text, "userid") !== false) {
+					$messages = [[
+						'type' => 'text',
+						'text' => $userId
+					]];
+				}
+				
+				if (!is_null($messages)) {
+				
+					// Make a POST Request to Messaging API to reply to sender
+					
+					$url = 'https://api.line.me/v2/bot/message/reply';
+					$data = [
+						'replyToken' => $replyToken,
+						'messages' => $messages,
+					];
+					$post = json_encode($data);
+					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+					$ch = curl_init($url);
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+					$result = curl_exec($ch);
+					curl_close($ch);
 
-			echo $result . "\r\n";
+					echo $result . "\r\n";
+				}
+				
+			}
 		}
+	}
+}
+function GetReplyMessage($text,$userId) {
+	$serviceUrl = 'http://vsmsdev.apps.thaibev.com/linebot/linebotWCF';
+	// Build message to reply back
+	if (stripos($text, "©та") !== false) {
+		$messages = [[
+		  'type'=> 'sticker',
+		  'packageId'=> '1',
+		  'stickerId'=> '3'
+		]];
 	}
 }
 echo "OK";
