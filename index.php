e0b9cf1c4c986eef16606366499e31a5fda77a88
chittawan <Associa_@hotmail.com>
2017-02-25 18:24:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,17 @@ $feedUrl = 'https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.xml';
 		$feedUrl = 'https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.xml';
 		$xml = simplexml_load_file($feedUrl);
 		$txt = '';
+		$myDate = '';
 		#echo $xml->weeklyevents->event->title;
 			foreach($xml->children() as $event)
 			{	 
-			   $impact = $event->impact;
+			   if($myDate != $event->date){
+				$myDate = $event->date;
+				$txt .= ($event->date) . "</br>";   
+			   }
+			   $myImpact = $event->impact;
 				#echo $impact;
-			   if($impact == 'High'){
+			   if($myImpact == 'High'){
 				$txt .= ($event->title) . "</br>";
 			   }
 			}
