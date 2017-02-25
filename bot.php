8b48b114fed13402ceffb92596d132d7c18140b8
chittawan <Associa_@hotmail.com>
2017-02-25 17:55:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -189,7 +189,7 @@ function GetReplyMessage($text,$userId) {
 			   $impact = $event->impact;
 				#echo $impact;
 			   if($impact == 'High'){
-				$txt += $event->title +'\r\n';
+				$txt = $txt + ($event->title) +'\r\n';
 			   }
 			}
 		$messages = [[
