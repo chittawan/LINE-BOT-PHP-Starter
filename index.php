0864289b516ec0156cd98135246b02ea6276c4e9
chittawan <Associa_@hotmail.com>
2017-02-25 17:28:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $xmlFeed = 'https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.xml';
 		$xml = simplexml_load_file($xmlFeed);
+print_r($xml);
 _dump($xml)
 		foreach($xml->weeklyevents->event as $key => $value)
 		{
