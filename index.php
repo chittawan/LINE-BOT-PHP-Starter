1bd84d15f9add8069b85c69b5ba0329410c950b7
chittawan <Associa_@hotmail.com>
2017-02-25 17:56:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-$xmlFeed = 'https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.xml';
-		$xml = simplexml_load_file($xmlFeed);
-
-echo $xml->weeklyevents->event->title;
-	foreach($xml->children() as $event)
-	{	 
-	   $impact = $event->impact;
-		#echo $impact;
-	   if($impact == 'High'){
-		echo $event->title;
-	   }
-	}
+$feedUrl = 'https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.xml';
+		$xml = simplexml_load_file($feedUrl);
+		$txt = '';
+		#echo $xml->weeklyevents->event->title;
+			foreach($xml->children() as $event)
+			{	 
+			   $impact = $event->impact;
+				#echo $impact;
+			   if($impact == 'High'){
+				$txt = $txt + ($event->title) +'\r\n';
+			   }
+			}
+echo $txt
 ?>
