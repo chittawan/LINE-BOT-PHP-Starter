150dccde599807aa67c51382f457d398a92de1ab
chittawan <Associa_@hotmail.com>
2017-02-27 13:16:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,7 +5,10 @@ $access_token = 'WrOvUObu3f++FH65SpmKQqkzd31q1HsVgv29G2EYPkye7NdGMp+I0/SeQHXIcje
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-$shortup = false;
+
+if (empty($shortup)) {
+        $shortup = false;
+    } 
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
