b903beabb53c631e6dbb62ec97f606bd9e0899b8
chittawan <Associa_@hotmail.com>
2017-02-28 02:59:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -217,24 +217,24 @@ function GetReplyMessage($text,$userId) {
 		$xml = simplexml_load_file($feedUrl);
 		$txt = '';
 		$myDate = '';
-		$myOldDate = '';
+		$myOldDate = DateTime::createFromFormat('d-m-Y', '01-01-2017');
 		#echo $xml->weeklyevents->event->title;
 			foreach($xml->children() as $event)
 			{	 
 			  $myDate = (string)$event->date;
 		          $myTime = (string)$event->time;
-			  $date = new DateTime($myDate . ' ' . $myTime);
-			  $date = $date->modify('+7 hours');
-			   if($myOldDate == '' || $date->format('d-m-Y') != $myOldDate->format('d-m-Y')){
-								
+			  $strTime = $myDate . ' ' . $myTime;
+			  $date =  DateTime::createFromFormat('d-m-Y H:ia', $strTime);			  
+			  $date->modify('+7 hours');		
+			  
+			  if((string)$date->format('d-m-Y') != (string)$myOldDate->format('d-m-Y')){
 				#echo $date->format('d-m-Y H:i:s');
 				#$myDate = $date->format('d-m-Y');
-				$txt .= $date->format('d-m-Y') . "\n";
+				$txt .= (string)$date->format('d-m-Y') . "\n";
 				$myOldDate = $date;
 			   }
-				#echo $impact;
 			   if($event->impact == 'High'){
-				$txt .= ($event->country) . ' ' . ($myOldDate->format('H:i:s')) . ' ' . ($event->title) . "\n";
+				$txt .= ($event->country) . ' ' . (string)($date->format('H:ia')) . ' ' . ($event->title) . "\n";
 			   }
 			}
 		           
