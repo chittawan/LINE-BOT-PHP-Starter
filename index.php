c7b358dfb8e465ab390882aad63e810b0b8302a8
chittawan <Associa_@hotmail.com>
2017-02-28 02:05:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $feedUrl = 'https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.xml?v=1';
 			  $date = new DateTime($myDate . ' ' . $myTime);
 			  $date = $date->modify('+7 hours');
 				echo $date->format('d-m-Y H:ia');
-			   if(strtotime($date) != strtotime($myOldDate)){
+			   if(strtotime($date) > strtotime($myOldDate)){
 							echo 'xxx';
 				#echo $date->format('d-m-Y H:i:s');
 				#$myDate = $date->format('d-m-Y');
