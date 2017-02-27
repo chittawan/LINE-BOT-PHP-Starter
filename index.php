91f543836349e60c48fac1be69ba5cb2be3d4d0b
chittawan <Associa_@hotmail.com>
2017-02-28 02:29:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $feedUrl = 'https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.xml?v=1';
 			  $date = new DateTime($strTime);
 			  #$date->modify('+7 hour');		
 			  
-			  echo $date->format('d-m-Y H:ia');
+			  echo $date->format('d-m-Y H:i:a');
 			   #if(1 == 1){
 				#echo $date->format('d-m-Y H:i:s');
 				#$myDate = $date->format('d-m-Y');
