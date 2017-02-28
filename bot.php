8d13a42618a8f85a15f8324799e6d70779827219
chittawan <Associa_@hotmail.com>
2017-02-28 11:01:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -207,6 +207,35 @@ function GetReplyMessage($text,$userId) {
 			'text' => "ค่า server โอนมาที่ \n 718-258-018-4 \n กสิกร \n วิทยา จงอุดมพร"
 		]];
 		
+	} else if (stripos($text, "Cfx xx") !== false) {	
+		$messages = [[
+					  "type"=> "template",
+					  "altText"=> "this is a buttons template",
+					  "template"=> [
+					      "type"=> "buttons",
+					      "thumbnailImageUrl"=> "https=>//example.com/bot/images/image.jpg",
+					      "title"=> "Menu",
+					      "text"=> "Please select",
+					      "actions"=> array(
+						  [
+						    "type"=> "postback",
+						    "label"=> "Buy",
+						    "data"=> "action=buy&itemid=123"
+						  ],
+						  [
+						    "type"=> "postback",
+						    "label"=> "Add to cart",
+						    "data"=> "action=add&itemid=123"
+						  ],
+						  [
+						    "type"=> "uri",
+						    "label"=> "View detail",
+						    "uri"=> "http=>//example.com/page/123"
+						  ]
+					      )
+					  ]
+		]];
+		
 	} else if (stripos($text, "Cfx Regis") !== false) {	
 		$messages = [[
 			'type' => 'text',
