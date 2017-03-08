08b4d740704ab0450fcbf199cd8d04e341cbab98
chittawan <Associa_@hotmail.com>
2017-03-08 16:42:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,6 +28,9 @@ if (!is_null($events['events'])) {
 			elseif ($event['source']['type'] == 'group') {
 				$userId = $event['source']['groupId'];			
 			}
+			elseif ($event['source']['type'] == 'room') {
+				$userId = $event['source']['roomId'];			
+			}
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -44,6 +47,19 @@ if (!is_null($events['events'])) {
 					'replyToken' => $replyToken,
 					'messages' => $messages
 				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
+				echo $result . "\r\n";
+				
+				$url = 'https://api.line.me/v2/bot/group/' . $userId . '/leave';
 				$post = json_encode($data);
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
