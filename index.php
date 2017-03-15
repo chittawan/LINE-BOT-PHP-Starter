7dec7fd7a498fb164e505338e7ad0978c266e48d
chittawan <Associa_@hotmail.com>
2017-03-16 02:44:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,7 @@
 <?php
 $groupId = 'Uce91bbcb4d5185a7c0ab1ebfdbd13539';
 $userId = 'E';
-function addWordFile($myUserId,$myAsk,$myAnswer){
-	
-	$myFileName = 'word_' . $myUserId . '.txt';
-	if(!file_exists($myFileName)){
-	   clearQuestionFile($myFileName);
-	}
-	if(file_exists($myFileName)){
-		$myfile = fopen($myFileName, "r") or die("Unable to open file!");		
-	 	$myArray = json_decode(fgets($myfile));		
-		if(empty($myArray)) $myArray = array();
-		fclose($myfile);
-		
-		$isExists = false;
-		foreach($myArray as $item)
-		{
-		    if($item->ask == $myAsk)
-		    {
-			$isExists = true;
-			$item->answer = $myAnswer;
-		    }
-		}
-		if(!$isExists){
-		  array_push($myArray,[
-			ask => $myAsk,
-			answer => $myAnswer
-			]);
-		}
-		echo $myAsk;
-		echo $myAnswer;
-		$json = json_encode($myArray, true);
-		if(file_exists($myFileName)){
-			$myfile = fopen($myFileName, "w") or die("Unable to open file!");
-			fwrite($myfile, $json);
-			fclose($myfile);
-			echo $json;
-			return 'OK.';
-		}
-	}
-	return 'Fail';
-}
-function clearQuestionFile($fileName){ 
-	$myfile = fopen($fileName, "w") or die("Unable to open file!");
-	fwrite($myfile, json_encode(array()));
-	fclose($myfile);
-	
-}
-$result =  addWordFile('Uce91bbcb4d5185a7c0ab1ebfdbd13539','Hello','Hi');
+$serviceUrl = 'http://webgis1.apps.thaibev.com/CheckService/CheckService.svc/ReadCheck';
+$result = GetWebService($serviceUrl);
 echo $result;
 ?>
