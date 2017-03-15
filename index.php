b26f49c23c254e0a6421f89d49b483c28909e212
chittawan <Associa_@hotmail.com>
2017-03-16 02:46:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 $groupId = 'Uce91bbcb4d5185a7c0ab1ebfdbd13539';
 $userId = 'E';
+
+function GetWebService($url) {
+	$headers = array('Content-Type: application/json');
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	
+	$jsonResult = json_decode($result, true);
+	
+	return $result;
+}
+
 $serviceUrl = 'http://webgis1.apps.thaibev.com/CheckService/CheckService.svc/ReadCheck';
 $result = GetWebService($serviceUrl);
 echo $result;
