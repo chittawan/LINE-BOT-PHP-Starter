9611c3b4cced232e3fa6b71e810a9e96ef0f3b15
chittawan <Associa_@hotmail.com>
2018-07-17 14:04:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -446,8 +446,8 @@ function GetReplyMessage($text,$myUserId) {
 				  "altText"=> "this is a carousel template",
 				  "template"=> [
 				      "type"=> "video",
-				   "originalContentUrl"=> "https://fathomless-anchorage-14853.herokuapp.com/playWd.mp4",
-				   "previewImageUrl"=> "https://fathomless-anchorage-14853.herokuapp.com/preview.jpg"
+				     //"originalContentUrl"=> "https://fathomless-anchorage-14853.herokuapp.com/playWd.mp4",
+				     //"previewImageUrl"=> "https://fathomless-anchorage-14853.herokuapp.com/preview.jpg"
 				     //"thumbnailImageUrl"=> "https://fathomless-anchorage-14853.herokuapp.com/WeddingPraeFilm.jpg",
 					    "title"=> "Wedding",
 					    "text"=> "รายละเอียด",
@@ -458,9 +458,9 @@ function GetReplyMessage($text,$myUserId) {
 						    "uri"=> "https://www.google.com/maps/place/wedding+and+conference+venue/@13.8753719,100.5806225,15.75z/data=!4m12!1m6!3m5!1s0x30e282d3bfc13d97:0xe03669df900ca167!2swedding+and+conference+venue!8m2!3d13.8739451!4d100.581968!3m4!1s0x30e282d3bfc13d97:0xe03669df900ca167!8m2!3d13.8739451!4d100.581968",
 						],
 						[
-						    "type"=> "message",
-						    "label"=> "ฉันไป",
-						    "text"=> "ฉันไป"
+						    "type"=> "uri",
+						    "label"=> "รับชม Teaser",
+						    "uri"=> "https://drive.google.com/open?id=1gKsTQTQI_2DIMJHQlS3CU-du7Wtql2cF"
 						]
 					    )
 				  ]
